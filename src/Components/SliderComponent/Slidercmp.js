754c9f4b778f0fe0cceefab23867c33d3e76bd2a
vahid Pourmohamad Shandiz <vahidpoormohamad@yahoo.co.uk>
2022-04-20 14:05:09 +0430

    6

--- src/Components/SliderComponent/Slidercmp.js
+++ src/Components/SliderComponent/Slidercmp.js
@@ -1,19 +1,14 @@
-import React from 'react'
-import './Slidercmp.css'
+import React from "react";
+import "./Slidercmp.css";
 import banner from "../../Assets/Images/banner-img.png";
 
 export default function Slidercmp() {
   return (
-      <div className='SlidercmpContainer'>
-          <div className='SlidercmpParent'>
-        <div className='SlidercmpImage'>
-          <img src={banner}></img>
-        </div>
-        <div className='SlidercmpText'>
-
-        </div>
-
-          </div>  
+    <div className="SlidercmpContainer">
+      <div className="SlidercmpParent">
+        <div className="SlidercmpImage">{/* <img src={banner}></img> */}</div>
+        <div className="SlidercmpText">یک خرید خیـــــلی عالی</div>
+      </div>
     </div>
-  )
+  );
 }
