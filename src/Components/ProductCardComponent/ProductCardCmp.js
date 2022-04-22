43b35387f7d0ccc544f636a1fefd14f4ea5df996
Vahid Pourmohammad Shandiz <vahidpoormohamad@yahoo.co.uk>
2022-04-22 22:01:23 +0430

    Development

--- src/Components/ProductCardComponent/ProductCardCmp.js
+++ src/Components/ProductCardComponent/ProductCardCmp.js
@@ -1,21 +1,21 @@
 import React from "react";
 import "./ProductCardCmp.css";
-import { CSSTransition } from 'react-transition-group';
+import { CSSTransition } from "react-transition-group";
 // src={`${process.env.PUBLIC_URL}/assets/project/bezanglogo.jpg`}
 export default function ProductCardCmp(props) {
   const { Id, Pname, Price, Discount, imgone, imgtwo } = props;
   return (
     <div className="productcard">
       <div className="pimg">
-              <div className="productimage1">
-                
+ 
+        <div className="productimage1">
           <img
             src={`${process.env.PUBLIC_URL}/assets/Products/` + imgone}
             style={{ width: "100%" }}
           ></img>
         </div>
         <div className="productimage2">
-          <img
+          <img className="test"
             src={`${process.env.PUBLIC_URL}/assets/Products/` + imgtwo}
             style={{ width: "100%" }}
           ></img>
