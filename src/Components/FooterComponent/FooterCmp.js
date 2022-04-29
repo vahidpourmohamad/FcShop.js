d54cea42aabd669b1cc9e77aa04d229df7ec3a60
Vahid Pourmohammad Shandiz <vahidpoormohamad@yahoo.co.uk>
2022-04-29 22:46:58 +0430

    Implemention Footer

--- src/Components/FooterComponent/FooterCmp.js
+++ src/Components/FooterComponent/FooterCmp.js
@@ -1,19 +1,98 @@
-import React from 'react'
-import './FooterCmp.css'
+import React from "react";
+import "./FooterCmp.css";
 export default function FooterCmp() {
   return (
     <footer className="footerContainer">
       <div className="upfooterribonborder">
         <div className="upfooterriboncontainer">
           <div className="footerriboniteamcontainer">
-            <div className="footerriboniteamimg"></div>
-                      <div className="footerriboniteamtext">
-                          <h2> سلام</h2>
+            <div className="footerriboniteamtext">
+              <div className="footerriboniteamtextHeading">
+                هدیه های بی نظیر
+              </div>
+              <div className="footerriboniteamtextSubHeading">الان ببرید</div>
+            </div>
+            <div className="footerriboniteamimg">
+              <i
+                href="#"
+                className="fa fa-solid fa-gift fa-3x"
+                style={{ color: "var(--main)" }}
+              ></i>
+            </div>
+          </div>
+          <div className="footerriboniteamcontainer">
+            <div className="footerriboniteamtext">
+              <div className="footerriboniteamtextHeading">حمل ونقل رایگان</div>
+              <div className="footerriboniteamtextSubHeading">حمل مطمئن</div>
+            </div>
+            <div className="footerriboniteamimg">
+              <li
+                href="#"
+                className="fa fa-solid fa-truck fa-3x "
+                style={{ color: "var(--main)" }}
+              ></li>
+            </div>
+          </div>
+          <div className="footerriboniteamcontainer">
+            <div className="footerriboniteamtext">
+              <div className="footerriboniteamtextHeading">پرداخت آنلاین</div>
+              <div className="footerriboniteamtextSubHeading">پرداخت امن</div>
+            </div>
+            <div className="footerriboniteamimg">
+              <li
+                href="#"
+                className="fa fa-solid fa-credit-card fa-3x "
+                style={{ color: "var(--main)" }}
+              ></li>
+            </div>
+          </div>
+          <div className="footerriboniteamcontainer">
+            <div className="footerriboniteamtext">
+              <div className="footerriboniteamtextHeading">پشتیبانی</div>
+              <div className="footerriboniteamtextSubHeading">
+                پشتیبانی سریع
+              </div>
+            </div>
+            <div className="footerriboniteamimg">
+              <li
+                href="#"
+                className="fa fa-solid fa-headphones fa-3x"
+                style={{ color: "var(--main)" }}
+              ></li>
             </div>
           </div>
         </div>
       </div>
-      <div className="FooterCmpcontainer"></div>
+      <div className="FooterCmpcontainer">
+        <div className="fiteam">
+          <div className="fiteamhead">نماد اعتماد الکترونیک</div>
+          <div className="fiteamline"></div>
+          <div className="fiteamtext">
+            <img
+              src={`${process.env.PUBLIC_URL}/assets/Banners/namad.jpg`}
+            ></img>
+          </div>
+        </div>
+        <div className="fiteam">
+          <div className="fiteamhead">لگو اصلی</div>
+          <div className="fiteamline"></div>
+          <div className="fiteamtext">
+            <img
+              src={`${process.env.PUBLIC_URL}/assets/Banners/namad.jpg`}
+            ></img>
+          </div>
+        </div>
+        <div className="fiteam">
+          <div className="fiteamhead">درباره ما</div>
+          <div className="fiteamline"></div>
+          <div className="fiteamtext">این متن آماده و سر خود است</div>
+        </div>
+        <div className="fiteam">
+          <div className="fiteamhead">منو</div>
+          <div className="fiteamline"></div>
+          <div className="fiteamtext">این منو هنوز آماده نشده است</div>
+        </div>
+      </div>
     </footer>
   );
 }
