be6d923cd7473d2846e832fa085f4a55b833d582
Vahid Pourmohammad Shandiz <vahidpoormohamad@yahoo.co.uk>
2022-05-11 20:13:28 +0430

    produckt

--- src/Seiten/ProduktSicht/ProduktSichtSeite.js
+++ src/Seiten/ProduktSicht/ProduktSichtSeite.js
@@ -5,20 +5,34 @@ import FooterCmp from "../../Components/FooterComponent/FooterCmp";
 import PromotionProduktenKomponente from "../../Components/PromotionProducts/Promotionproductscmp";
 import { useParams } from "react-router-dom";
 import "./ProduktSichtSeite.css";
+import { dummy } from "./dummy";
 
 export default function ProduktSichtSeite() {
   const params = useParams();
-  console.log("test");
-  console.log(params);
+  const {name,price,desc,colors,category,status,images}= dummy
   return (
     <Fragment>
       <MenuCmp />
       <div className="MenüHintergrund"></div>
-      <div className="ProduktKörper">
-        <div className="ProduktBilds">test</div>
-        <div className="ProduktkurzehBeschreibung">test</div>
+      <div className="Container-Center">
+        <div className="ProduktKörper">
+          <div className="ProduktBilds">test</div>
+          <div className="ProduktkurzehBeschreibung">
+            <div className="font-m">دسترسی</div>
+            <div className="font-xxl">{name}</div>
+
+            <div className="font-m">{price}</div>
+
+            <div className="font-l TextRTl">{desc}</div>
+            <div className="Zweihintereinader">
+              <div className="font-s">رنگ</div>
+              <div className="font-s">رنگ</div>
+            </div>
+            <div className="font-s">دسته بندی</div>
+          </div>
+        </div>
       </div>
-      <PromotionProduktenKomponente />
+      <PromotionProduktenKomponente BackgroundImage="../../assets/Banners/bg.jpg" />
       <FooterCmp />
     </Fragment>
   );
